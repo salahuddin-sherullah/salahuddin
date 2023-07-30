c10e935ed5b56d861db33153448bb90031351752
Salahuddin <salahuddin.iba@gmail.com>
2023-07-30 06:51:30 -0700

    updated pages

--- js/init.js
+++ js/init.js
@@ -1,122 +1,116 @@
-/*
-	Ion by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 (function($) {
 
-	skel.init({
-		reset: 'full',
-		breakpoints: {
-			
-			// Global.
-				global: {
-					range: '*',
-					href: 'css/style.css',
-					containers: 1400,
-					grid: {
-						gutters: {
-							vertical: '4em',
-							horizontal: 0
-						}
-					}
-				},
+    skel.init({
+        reset: 'full',
+        breakpoints: {
 
-			// XLarge.
-				xlarge: {
-					range: '-1680',
-					href: 'css/style-xlarge.css',
-					containers: 1200
-				},
+            // Global.
+            global: {
+                range: '*',
+                href: 'css/style.css',
+                containers: 1400,
+                grid: {
+                    gutters: {
+                        vertical: '4em',
+                        horizontal: 0
+                    }
+                }
+            },
 
-			// Large.
-				large: {
-					range: '-1280',
-					href: 'css/style-large.css',
-					containers: 960,
-					grid: {
-						gutters: {
-							vertical: '2.5em'
-						}
-					},
-					viewport: {
-						scalable: false
-					}
-				},
+            // XLarge.
+            xlarge: {
+                range: '-1680',
+                href: 'css/style-xlarge.css',
+                containers: 1200
+            },
 
-			// Medium.
-				medium: {
-					range: '-980',
-					href: 'css/style-medium.css',
-					containers: '90%',
-					grid: {
-						collapse: 1
-					}
-				},
+            // Large.
+            large: {
+                range: '-1280',
+                href: 'css/style-large.css',
+                containers: 960,
+                grid: {
+                    gutters: {
+                        vertical: '2.5em'
+                    }
+                },
+                viewport: {
+                    scalable: false
+                }
+            },
 
-			// Small.
-				small: {
-					range: '-736',
-					href: 'css/style-small.css',
-					containers: '90%',
-					grid: {
-						gutters: {
-							vertical: '1.25em'
-						}
-					}
-				},
+            // Medium.
+            medium: {
+                range: '-980',
+                href: 'css/style-medium.css',
+                containers: '90%',
+                grid: {
+                    collapse: 1
+                }
+            },
 
-			// XSmall.
-				xsmall: {
-					range: '-480',
-					href: 'css/style-xsmall.css',
-					grid: {
-						collapse: 2
-					}
-				}
+            // Small.
+            small: {
+                range: '-736',
+                href: 'css/style-small.css',
+                containers: '90%',
+                grid: {
+                    gutters: {
+                        vertical: '1.25em'
+                    }
+                }
+            },
 
-		},
-		plugins: {
-			layers: {
-				
-				// Config.
-					config: {
-						transform: true
-					},
-				
-				// Navigation Panel.
-					navPanel: {
-						animation: 'pushX',
-						breakpoints: 'medium',
-						clickToHide: true,
-						height: '100%',
-						hidden: true,
-						html: '<div data-action="moveElement" data-args="nav"></div>',
-						orientation: 'vertical',
-						position: 'top-left',
-						side: 'left',
-						width: 250
-					},
+            // XSmall.
+            xsmall: {
+                range: '-480',
+                href: 'css/style-xsmall.css',
+                grid: {
+                    collapse: 2
+                }
+            }
 
-				// Navigation Button.
-					navButton: {
-						breakpoints: 'medium',
-						height: '4em',
-						html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
-						position: 'top-left',
-						side: 'top',
-						width: '6em'
-					}
+        },
+        plugins: {
+            layers: {
 
-			}
-		}
-	});
+                // Config.
+                config: {
+                    transform: true
+                },
 
-	$(function() {
-		
-		// jQuery ready stuff.
-		
-	});
+                // Navigation Panel.
+                navPanel: {
+                    animation: 'pushX',
+                    breakpoints: 'medium',
+                    clickToHide: true,
+                    height: '100%',
+                    hidden: true,
+                    html: '<div data-action="moveElement" data-args="nav"></div>',
+                    orientation: 'vertical',
+                    position: 'top-left',
+                    side: 'left',
+                    width: 250
+                },
+
+                // Navigation Button.
+                navButton: {
+                    breakpoints: 'medium',
+                    height: '4em',
+                    html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span>',
+                    position: 'top-left',
+                    side: 'top',
+                    width: '6em'
+                }
+
+            }
+        }
+    });
+
+    $(function() {
+
+        // jQuery ready stuff.
+
+    });
 
 })(jQuery);
